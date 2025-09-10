9f401c4a0eb36bd31a38a1f21712c6d0ed414a38
Chloeruebennett <dzdz67204@gmail.com>
2025-09-10 18:44:03 +0500

    Update image_generator.py

--- modules/image_generator.py
+++ modules/image_generator.py
@@ -5,12 +5,10 @@ import os
 class ImageGenerator:
     @staticmethod
     def generate_image(value):
-        # Создаем изображение с уникальным паттерном на основе числа
         size = (256, 256)
         img = Image.new("RGB", size, color=(255, 255, 255))
         draw = ImageDraw.Draw(img)
 
-        # Генерация паттерна
         for i in range(0, size[0], 20):
             color_value = int((value * i) % 255)
             draw.line([(i, 0), (i, size[1])], fill=(color_value, 100, 150), width=2)
